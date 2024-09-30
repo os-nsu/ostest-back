509701367a02f1afbdc6e028b1711fa1c6d3549f
anastasiya <nastya050620031n@gmail.com>
2024-09-30 23:18:34 +0700

    refs #12192 Disabled tests

--- src/test/java/ru/nsu/ostest/LaboratoryCreationIntegrationTest.java
+++ src/test/java/ru/nsu/ostest/LaboratoryCreationIntegrationTest.java
@@ -2,6 +2,7 @@ package ru.nsu.ostest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Disabled
 public class LaboratoryCreationIntegrationTest {
 
     private static final String URL = "/api/laboratory";

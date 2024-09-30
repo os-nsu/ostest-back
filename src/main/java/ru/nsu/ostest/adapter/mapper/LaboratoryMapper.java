491086c4293084917e5bdb669e9906876448698b
anastasiya <nastya050620031n@gmail.com>
2024-09-30 21:42:51 +0700

    refs #12193 Fixed mapping

--- src/main/java/ru/nsu/ostest/adapter/mapper/LaboratoryMapper.java
+++ src/main/java/ru/nsu/ostest/adapter/mapper/LaboratoryMapper.java
@@ -1,50 +1,23 @@
 package ru.nsu.ostest.adapter.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 import org.mapstruct.control.DeepClone;
-import org.mapstruct.factory.Mappers;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryCreationRequestDto;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryDto;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryShortDto;
-import ru.nsu.ostest.adapter.in.rest.model.test.TestDto;
 import ru.nsu.ostest.adapter.out.persistence.entity.laboratory.Laboratory;
-import ru.nsu.ostest.adapter.out.persistence.entity.test.Test;
-import ru.nsu.ostest.adapter.out.persistence.entity.test.TestLaboratoryLink;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Mapper(
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         componentModel = MappingConstants.ComponentModel.SPRING,
-        mappingControl = DeepClone.class
+        mappingControl = DeepClone.class,
+        uses = TestMapper.class
 )
 public interface LaboratoryMapper {
     Laboratory laboratoryCreationRequestDtoToLaboratory(LaboratoryCreationRequestDto laboratoryCreationRequestDto);
 
-    default LaboratoryDto laboratoryToLaboratoryDto(Laboratory laboratory) {
-        if (laboratory == null) {
-            return null;
-        }
-
-        Long id = laboratory.getId();
-        String name = laboratory.getName();
-        String description = laboratory.getDescription();
-        Integer semesterNumber = laboratory.getSemesterNumber();
-        LocalDateTime deadline = laboratory.getDeadline();
-        Boolean isHidden = laboratory.getIsHidden();
-
-        List<Test> tests = laboratory.getTestsLinks().stream()
-                .map(TestLaboratoryLink::getTest)
-                .toList();
-        List<TestDto> testsDto = tests.stream()
-                .map((t) -> Mappers.getMapper(TestMapper.class).toTestDtoFromEntity(t))
-                .toList();
-
-        return new LaboratoryDto(id, name, description, semesterNumber, deadline, isHidden, testsDto);
-    }
+    @Mapping(target = "tests", source = "testsLinks")
+    LaboratoryDto laboratoryToLaboratoryDto(Laboratory laboratory);
 
     LaboratoryShortDto laboratoryToLaboratoryShortDto(Laboratory laboratory);
 }
--- src/main/java/ru/nsu/ostest/adapter/mapper/TestMapper.java
+++ src/main/java/ru/nsu/ostest/adapter/mapper/TestMapper.java
@@ -1,9 +1,11 @@
 package ru.nsu.ostest.adapter.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.nsu.ostest.adapter.in.rest.model.test.TestCreationRequestDto;
 import ru.nsu.ostest.adapter.in.rest.model.test.TestDto;
 import ru.nsu.ostest.adapter.out.persistence.entity.test.Test;
+import ru.nsu.ostest.adapter.out.persistence.entity.test.TestLaboratoryLink;
 
 @Mapper(componentModel = "spring")
 public interface TestMapper {
@@ -12,4 +14,10 @@ public interface TestMapper {
     Test toTest(TestDto testDto);
 
     TestDto toTestDtoFromEntity(Test test);
+
+    @Mapping(source = "testLaboratoryLink.test.id", target = "id")
+    @Mapping(source = "testLaboratoryLink.test.name", target = "name")
+    @Mapping(source = "testLaboratoryLink.test.description", target = "description")
+    @Mapping(source = "testLaboratoryLink.test.category", target = "testCategory")
+    TestDto fromTestLaboratoryLink(TestLaboratoryLink testLaboratoryLink);
 }

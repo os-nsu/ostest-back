1faef7e4559c5cca8203dab297dc0b8621c3969a
anastasiya <nastya050620031n@gmail.com>
2024-10-02 17:31:02 +0700

    refs #12214 Added new tests for laboratory edition and fixed the other ones

--- src/test/java/ru/nsu/ostest/LaboratoryControllerIntegrationTest.java
+++ src/test/java/ru/nsu/ostest/LaboratoryControllerIntegrationTest.java
@@ -12,13 +12,13 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryCreationRequestDto;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryDto;
+import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryEditionRequestDto;
 import ru.nsu.ostest.domain.repository.LaboratoryRepository;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Disabled
 public class LaboratoryControllerIntegrationTest {
 
-    private static final String URL = "/api/laboratory";
+    private static final String CREATE_OR_EDIT_URL = "/api/laboratory";
 
     @Autowired
     private MockMvc mockMvc;
@@ -53,13 +53,14 @@ public class LaboratoryControllerIntegrationTest {
         LaboratoryCreationRequestDto request =
                 new LaboratoryCreationRequestDto(name, description, semesterNumber, dateTime, isHidden);
 
-        mockMvc.perform(post(URL)
+        mockMvc.perform(post(CREATE_OR_EDIT_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name").value("Test Laboratory"))
-                .andExpect(jsonPath("$.description").value("Test Description"))
-                .andExpect(jsonPath("$.semesterNumber").value("1"));
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.description").value(description))
+                .andExpect(jsonPath("$.semesterNumber").value(semesterNumber))
+                .andExpect(jsonPath("$.isHidden").value(isHidden));
     }
 
     @Test
@@ -72,7 +73,7 @@ public class LaboratoryControllerIntegrationTest {
         LaboratoryCreationRequestDto request1 =
                 new LaboratoryCreationRequestDto(name, description, semesterNumber, dateTime, isHidden);
 
-        mockMvc.perform(post(URL)
+        mockMvc.perform(post(CREATE_OR_EDIT_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request1)))
                 .andExpect(status().isCreated());
@@ -84,7 +85,7 @@ public class LaboratoryControllerIntegrationTest {
         LaboratoryCreationRequestDto request2 =
                 new LaboratoryCreationRequestDto(name, description, semesterNumber, dateTime, isHidden);
 
-        mockMvc.perform(post(URL)
+        mockMvc.perform(post(CREATE_OR_EDIT_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request2)))
                 .andExpect(status().isBadRequest());
@@ -100,13 +101,16 @@ public class LaboratoryControllerIntegrationTest {
         LaboratoryCreationRequestDto request =
                 new LaboratoryCreationRequestDto(name, description, semesterNumber, dateTime, isHidden);
 
-        MockHttpServletResponse mvcResponse = mockMvc.perform(post(URL)
+        MockHttpServletResponse mvcResponse = mockMvc.perform(post(CREATE_OR_EDIT_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name").value("Test Laboratory to Delete"))
-                .andExpect(jsonPath("$.description").value("Test Description"))
-                .andExpect(jsonPath("$.semesterNumber").value(1)).andReturn().getResponse();
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.description").value(description))
+                .andExpect(jsonPath("$.semesterNumber").value(semesterNumber))
+                .andExpect(jsonPath("$.isHidden").value(isHidden))
+                .andReturn()
+                .getResponse();
 
         LaboratoryDto laboratory =
                 objectMapper.createParser(mvcResponse.getContentAsByteArray()).readValueAs(LaboratoryDto.class);
@@ -120,4 +124,93 @@ public class LaboratoryControllerIntegrationTest {
 
         assertNull(laboratoryRepository.findById(laboratoryId).orElse(null));
     }
+
+    @Test
+    public void editLaboratory_ShouldReturnCreated_WhenValidRequest() throws Exception {
+        String name = "Test Laboratory to Edit";
+        String description = "Test Description Not Edited";
+        Integer semesterNumber = 1;
+        LocalDateTime dateTime = LocalDateTime.now().plusDays(7);
+        boolean isHidden = false;
+        LaboratoryCreationRequestDto creationRequest =
+                new LaboratoryCreationRequestDto(name, description, semesterNumber, dateTime, isHidden);
+
+        MockHttpServletResponse mvcResponse = mockMvc.perform(post(CREATE_OR_EDIT_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(creationRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.description").value(description))
+                .andExpect(jsonPath("$.semesterNumber").value(semesterNumber))
+                .andExpect(jsonPath("$.isHidden").value(isHidden))
+                .andReturn()
+                .getResponse();
+
+        description = "Edited Test Description";
+        isHidden = true;
+
+        LaboratoryDto laboratory =
+                objectMapper.createParser(mvcResponse.getContentAsByteArray()).readValueAs(LaboratoryDto.class);
+
+        Long laboratoryId = laboratory.id();
+
+        LaboratoryEditionRequestDto editionRequest =
+                new LaboratoryEditionRequestDto(laboratoryId, name, description, semesterNumber, dateTime, isHidden);
+
+        mockMvc.perform(put(CREATE_OR_EDIT_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(editionRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.description").value(description))
+                .andExpect(jsonPath("$.semesterNumber").value(semesterNumber))
+                .andExpect(jsonPath("$.isHidden").value(isHidden));
+    }
+
+    @Test
+    public void editLaboratory_ShouldReturnConflict_WhenDuplicateName() throws Exception {
+        String duplicatedName = "Duplicate Laboratory to Edit";
+        String description = "Test Description Not Edited";
+        int semesterNumber = 1;
+        LocalDateTime dateTime = LocalDateTime.now().plusDays(7);
+        boolean isHidden = false;
+        LaboratoryCreationRequestDto creationRequest =
+                new LaboratoryCreationRequestDto(duplicatedName, description, semesterNumber, dateTime, isHidden);
+
+        mockMvc.perform(post(CREATE_OR_EDIT_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(creationRequest)))
+                .andExpect(status().isCreated());
+
+        String name = "Laboratory to Edit";
+        semesterNumber = 4;
+        dateTime = LocalDateTime.now().plusDays(7);
+        isHidden = true;
+
+        creationRequest =
+                new LaboratoryCreationRequestDto(name, description, semesterNumber, dateTime, isHidden);
+
+        MockHttpServletResponse mvcResponse = mockMvc.perform(post(CREATE_OR_EDIT_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(creationRequest)))
+                .andExpect(status().isCreated()).andReturn().getResponse();
+
+        description = "Edited Test Description";
+        isHidden = false;
+
+        LaboratoryDto laboratory =
+                objectMapper.createParser(mvcResponse.getContentAsByteArray()).readValueAs(LaboratoryDto.class);
+
+        Long laboratoryId = laboratory.id();
+
+        LaboratoryEditionRequestDto editionRequest =
+                new LaboratoryEditionRequestDto(laboratoryId, duplicatedName, description,
+                        semesterNumber, dateTime, isHidden);
+
+        mockMvc.perform(put(CREATE_OR_EDIT_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(editionRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
 }

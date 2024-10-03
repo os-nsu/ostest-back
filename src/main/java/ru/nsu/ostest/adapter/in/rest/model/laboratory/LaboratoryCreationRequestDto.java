953921c9d3ab09830efb5ef62768abc3aab8f3ff
Roman Yatmanov <30433303+Yattroman@users.noreply.github.com>
2024-10-03 21:08:37 +0700

    refs #12457 Adds integration test example (#10)

--- src/main/java/ru/nsu/ostest/adapter/in/rest/model/laboratory/LaboratoryCreationRequestDto.java
+++ src/main/java/ru/nsu/ostest/adapter/in/rest/model/laboratory/LaboratoryCreationRequestDto.java
@@ -1,7 +1,10 @@
 package ru.nsu.ostest.adapter.in.rest.model.laboratory;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record LaboratoryCreationRequestDto(String name,
                                            String description,
                                            Integer semesterNumber,

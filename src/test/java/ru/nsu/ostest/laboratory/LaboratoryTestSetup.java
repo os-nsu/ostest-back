536182d1ed78c7a13e20028996ddf6d9c6d71d12
Анастасия Cтещенко <90607535+nastyasteshchenko@users.noreply.github.com>
2024-10-07 18:20:16 +0700

    refs #12458 Fixed laboratory tests (#11)
    * refs #12458 Fixed laboratory tests
    
    * refs #12458 Removed assert

--- src/test/java/ru/nsu/ostest/laboratory/LaboratoryTestSetup.java
+++ src/test/java/ru/nsu/ostest/laboratory/LaboratoryTestSetup.java
@@ -9,14 +9,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryCreationRequestDto;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryDto;
+import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryEditionRequestDto;
 import ru.nsu.ostest.domain.repository.LaboratoryRepository;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -52,10 +52,39 @@ public class LaboratoryTestSetup {
         mockMvc.perform(post(PATH)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(creationRequestDto)))
-                .andExpect(status().isBadRequest())
-                .andReturn();
+                .andExpect(status().isBadRequest());
 
         assertEquals(1, laboratoryRepository.findAll().size());
+    }
+
+    public void deleteLaboratory(Long labToDeleteId) throws Exception {
+        mockMvc.perform(delete("/api/laboratory/{id}", labToDeleteId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(labToDeleteId)))
+                .andExpect(status().isOk());
+
+        assertFalse(laboratoryRepository.findById(labToDeleteId).isPresent());
+    }
+
+    public LaboratoryDto editLaboratory(LaboratoryEditionRequestDto laboratoryEditionRequestDto) throws Exception {
+        var result = mockMvc.perform(put(PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(laboratoryEditionRequestDto)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var laboratory = objectMapper.readValue(result.getResponse().getContentAsString(), LaboratoryDto.class);
+
+        assertTrue(laboratoryRepository.findById(laboratory.id()).isPresent());
+
+        return laboratory;
+    }
+
+    public void editLaboratoryBad(LaboratoryEditionRequestDto laboratoryEditionRequestDto) throws Exception {
+        mockMvc.perform(put(PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(laboratoryEditionRequestDto)))
+                .andExpect(status().isBadRequest());
     }
 
     public LaboratoryDto getLaboratoryDto(String path) throws IOException {

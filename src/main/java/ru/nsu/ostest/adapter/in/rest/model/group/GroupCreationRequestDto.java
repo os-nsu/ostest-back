5dcbbdef7b372a21d707d6999bef5569dac09bf7
MariiaKataeva <mariiakataeva@gmail.com>
2024-10-12 20:38:32 +0700

    refs #12404 deleted users from GroupCreationRequestDto and GroupEditionRequestDto

--- src/main/java/ru/nsu/ostest/adapter/in/rest/model/group/GroupCreationRequestDto.java
+++ src/main/java/ru/nsu/ostest/adapter/in/rest/model/group/GroupCreationRequestDto.java
@@ -1,9 +1,6 @@
 package ru.nsu.ostest.adapter.in.rest.model.group;
 
-import java.util.List;
-
 public record GroupCreationRequestDto(
-        String name,
-        List<Long> usersIds
+        String name
 ) {
 }
--- src/main/java/ru/nsu/ostest/adapter/in/rest/model/group/GroupEditionRequestDto.java
+++ src/main/java/ru/nsu/ostest/adapter/in/rest/model/group/GroupEditionRequestDto.java
@@ -1,11 +1,8 @@
 
 package ru.nsu.ostest.adapter.in.rest.model.group;
 
-import java.util.List;
-
 public record GroupEditionRequestDto(
         Long id,
-        String name,
-        List<Long> usersIds
+        String name
 ) {
 }

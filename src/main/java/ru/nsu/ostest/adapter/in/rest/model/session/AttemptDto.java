13df6ce7b466f52b1babed16555a00b6881867b5
anastasiya <nastya050620031n@gmail.com>
2024-10-31 23:42:08 +0700

    refs #12967 Edited Attempt entity

--- src/main/java/ru/nsu/ostest/adapter/in/rest/model/session/AttemptDto.java
+++ src/main/java/ru/nsu/ostest/adapter/in/rest/model/session/AttemptDto.java
@@ -2,8 +2,10 @@ package ru.nsu.ostest.adapter.in.rest.model.session;
 
 import ru.nsu.ostest.domain.common.model.TestResults;
 
+import java.util.UUID;
+
 public record AttemptDto(
-        Long id,
+        UUID id,
         String name,
         TestResults testResults
 ) {
---
+++ src/main/java/ru/nsu/ostest/domain/common/enums/AttemptStatus.java
@@ -0,0 +1,5 @@
+package ru.nsu.ostest.domain.common.enums;
+
+public enum AttemptStatus {
+    IN_QUEUE
+}

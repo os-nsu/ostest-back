77e2d867d0b0912bb8554859754cb9133f4cda11
Пашкова Алина Сергеевна <al.passshkova@gmail.com>
2024-11-12 19:52:28 +0700

    refs #12642 Refactored pagination into class

--- src/main/java/ru/nsu/ostest/adapter/in/rest/model/filter/Pagination.java
+++ src/main/java/ru/nsu/ostest/adapter/in/rest/model/filter/Pagination.java
@@ -1,14 +1,21 @@
 package ru.nsu.ostest.adapter.in.rest.model.filter;
 
+import lombok.Data;
 import org.springframework.data.domain.Page;
 
-public record Pagination(Integer index, Integer pageSize, Integer totalRecords,
-                         Integer totalPages) {
-    public Pagination {
-        if (index == null) index = 1;
-        if (pageSize == null) pageSize = 20;
-    }
+@Data
+public class Pagination {
+    private final Integer index;
+    private final Integer pageSize;
+    private final Integer totalRecords;
+    private final Integer totalPages;
 
+    public Pagination(Integer index, Integer pageSize, Integer totalRecords, Integer totalPages) {
+        this.index = (index == null) ? 1 : index;
+        this.pageSize = (pageSize == null) ? 20 : pageSize;
+        this.totalRecords = totalRecords;
+        this.totalPages = totalPages;
+    }
     public static <T> Pagination createPagination(Page<T> page) {
         return new Pagination(
                 page.getNumber() + 1,

d326131b740dd6e61fdf8df423e411b6831b6824
Анастасия Cтещенко <90607535+nastyasteshchenko@users.noreply.github.com>
2024-11-14 00:36:30 +0700

    refs #13393 Minor session fixes (#36)
    * refs #13393 Fixed session (without pagination)
    
    * refs #13393 Added pagination for getting users' sessions
    
    * refs #13393 Added session mapper for test

--- src/main/java/ru/nsu/ostest/adapter/in/rest/controller/SessionController.java
+++ src/main/java/ru/nsu/ostest/adapter/in/rest/controller/SessionController.java
@@ -1,12 +1,14 @@
 package ru.nsu.ostest.adapter.in.rest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.nsu.ostest.adapter.in.rest.model.session.*;
 import ru.nsu.ostest.domain.service.SessionService;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -28,13 +30,14 @@ public class SessionController {
     }
 
     @PostMapping("/lab-student")
-    public SessionDto getLabSessionForStudent(@RequestBody GetLabSessionFromStudentRequestDto request) {
+    public SessionShortDto getLabSessionForStudent(@RequestBody GetLabSessionFromStudentRequestDto request) {
         return sessionService.getLabSessionForStudent(request);
     }
 
     @GetMapping("/user/{userId}")
-    public List<SessionDto> getUserSessions(@PathVariable Long userId) {
-        return sessionService.getUserSessions(userId);
+    public Page<SessionShortDto> getUserSessions(@PathVariable Long userId,
+                                                 @PageableDefault(size = 100) Pageable pageable) {
+        return sessionService.getUserSessions(userId, pageable);
     }
 
     @PostMapping("/{sessionId}/attempt")
--- src/main/java/ru/nsu/ostest/domain/service/SessionService.java
+++ src/main/java/ru/nsu/ostest/domain/service/SessionService.java
@@ -1,6 +1,8 @@
 package ru.nsu.ostest.domain.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.nsu.ostest.adapter.in.rest.model.session.*;
@@ -10,6 +12,7 @@ import ru.nsu.ostest.adapter.out.persistence.entity.laboratory.Laboratory;
 import ru.nsu.ostest.adapter.out.persistence.entity.session.Attempt;
 import ru.nsu.ostest.adapter.out.persistence.entity.session.Session;
 import ru.nsu.ostest.adapter.out.persistence.entity.user.User;
+import ru.nsu.ostest.domain.common.enums.SessionStatus;
 import ru.nsu.ostest.domain.exception.validation.LaboratoryNotFoundException;
 import ru.nsu.ostest.domain.exception.validation.SessionNotFoundException;
 import ru.nsu.ostest.domain.exception.validation.UserNotFoundException;
@@ -20,7 +23,6 @@ import ru.nsu.ostest.domain.repository.LaboratoryRepository;
 import ru.nsu.ostest.domain.repository.SessionRepository;
 import ru.nsu.ostest.domain.repository.UserRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -44,6 +46,7 @@ public class SessionService {
 
         Session session = new Session();
         session.setStudent(student);
+        session.setStatus(SessionStatus.NEW);
         session.setLaboratory(laboratory);
 
         session = sessionRepository.save(session);
@@ -57,15 +60,16 @@ public class SessionService {
         return sessionMapper.sessionToSessionDto(session);
     }
 
-    public SessionDto getLabSessionForStudent(GetLabSessionFromStudentRequestDto getLabSessionFromStudentRequestDto) {
+    public SessionShortDto getLabSessionForStudent(GetLabSessionFromStudentRequestDto getLabSessionFromStudentRequestDto) {
         Long studentId = getLabSessionFromStudentRequestDto.studentId();
         Long laboratoryId = getLabSessionFromStudentRequestDto.laboratoryId();
         Session session = sessionRepository.getSessionByStudentIdAndLaboratoryId(studentId, laboratoryId);
-        return sessionMapper.sessionToSessionDto(session);
+        return sessionMapper.sessionToSessionShortDto(session);
     }
 
-    public List<SessionDto> getUserSessions(Long userId) {
-        return sessionMapper.sessionToSessionDto(sessionRepository.getSessionByStudentIdOrTeacherId(userId, userId));
+    public Page<SessionShortDto> getUserSessions(Long userId, Pageable pageRequest) {
+        return sessionRepository.getSessionByStudentIdOrTeacherId(userId, userId, pageRequest)
+                .map(sessionMapper::sessionToSessionShortDto);
     }
 
     @Transactional
@@ -75,6 +79,10 @@ public class SessionService {
         Attempt attempt = attemptMapper.makeAttemptDtoToAttempt(makeAttemptDto);
         attempt = session.makeAttempt(attempt);
         attempt = attemptRepository.save(attempt);
+        if (session.getStatus().equals(SessionStatus.NEW)) {
+            session.setStatus(SessionStatus.IN_PROGRESS);
+            sessionRepository.save(session);
+        }
         return attemptMapper.attemptToAttemptDto(attempt);
     }
 

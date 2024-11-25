68f9876da41735bacc42e089569c5654b03ac9bd
Анастасия Cтещенко <90607535+nastyasteshchenko@users.noreply.github.com>
2024-11-25 20:42:48 +0700

    refs #13651 Implements general setup for tests (#44)
    * refs #13651 Started using TestTestSetup in LaboratoryControllerIntegrationTest
    
    * refs #13651 Started using UserTestSetup and LaboratoryTestSetup in SessionControllerIntegrationTest
    
    * refs #13651 Renamed constants
    
    * refs #13651 Renamed field with name 'setUpIsDone'
    
    * refs #13651 Fixed after review

--- src/test/java/ru/nsu/ostest/session/SessionControllerIntegrationTest.java
+++ src/test/java/ru/nsu/ostest/session/SessionControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package ru.nsu.ostest.session;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,54 +8,49 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import ru.nsu.ostest.TransactionalHelper;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryCreationRequestDto;
+import ru.nsu.ostest.adapter.in.rest.model.laboratory.LaboratoryDto;
 import ru.nsu.ostest.adapter.in.rest.model.session.*;
 import ru.nsu.ostest.adapter.in.rest.model.user.role.RoleEnum;
 import ru.nsu.ostest.adapter.in.rest.model.user.userData.UserCreationRequestDto;
-import ru.nsu.ostest.adapter.mapper.LaboratoryMapper;
-import ru.nsu.ostest.adapter.mapper.UserMapper;
+import ru.nsu.ostest.adapter.in.rest.model.user.userData.UserDto;
 import ru.nsu.ostest.adapter.out.persistence.entity.group.Group;
-import ru.nsu.ostest.adapter.out.persistence.entity.laboratory.Laboratory;
-import ru.nsu.ostest.adapter.out.persistence.entity.user.User;
 import ru.nsu.ostest.domain.repository.GroupRepository;
-import ru.nsu.ostest.domain.repository.LaboratoryRepository;
 import ru.nsu.ostest.domain.repository.SessionRepository;
-import ru.nsu.ostest.domain.repository.UserRepository;
+import ru.nsu.ostest.laboratory.LaboratoryTestSetup;
+import ru.nsu.ostest.security.impl.JwtAuthentication;
+import ru.nsu.ostest.user.UserTestSetup;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 @Testcontainers
 @AutoConfigureMockMvc(addFilters = false)
-@Import({SessionTestSetup.class})
+@Import({SessionTestSetup.class, LaboratoryTestSetup.class, UserTestSetup.class})
 @SpringBootTest
 public class SessionControllerIntegrationTest {
-    private static final String USER_NAME = "username";
-    private static final String FIRST_NAME = "firstName";
-    private static final String SECOND_NAME = "secondName";
     private static final String GROUP_NUMBER = "21207";
-    private static final String LAB_NAME = "Laboratory";
-    private static final String LAB_DESCRIPTION = "Description";
-    private static final boolean IS_HIDDEN = false;
-    private static final int SEMESTER_NUMBER = 1;
     private static final String REPOSITORY_URL = "Url";
     private static final String BRANCH_NAME = "Branch";
-    private static final OffsetDateTime DEADLINE = OffsetDateTime.parse("2024-10-07T07:02:27Z");
     private static final String SESSION_USER1_LAB1_DTO = "session/session_user1_lab1.json";
     private static final String SESSION_USER2_LAB2_DTO = "session/session_user2_lab2.json";
     private static final String ATTEMPT1_DTO = "session/attempt1.json";
     private static final String ATTEMPT2_DTO = "session/attempt2.json";
+    private static final List<UserDto> STUDENTS = new ArrayList<>();
+    private static final List<LaboratoryDto> LABORATORIES = new ArrayList<>();
 
     @Container
     @ServiceConnection
@@ -63,77 +59,74 @@ public class SessionControllerIntegrationTest {
     );
 
     @Autowired
-    private LaboratoryMapper laboratoryMapper;
-
-    @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
     private SessionTestMapper sessionTestMapper;
 
     @Autowired
-    private LaboratoryRepository laboratoryRepository;
-
-    @Autowired
     private SessionRepository sessionRepository;
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private GroupRepository groupRepository;
 
     @Autowired
     private SessionTestSetup sessionTestSetup;
-    private final List<User> students = new ArrayList<>();
-    private final List<Laboratory> laboratories = new ArrayList<>();
 
     @Autowired
-    private TransactionalHelper transactionalHelper;
+    private LaboratoryTestSetup laboratoryTestSetup;
+
+    @Autowired
+    private UserTestSetup userTestSetup;
+    private static boolean SET_UP_IS_DONE = false;
 
     @BeforeEach
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     void init() {
-        transactionalHelper.runInTransaction(this::initInTransaction);
-    }
-
-    void initInTransaction() {
-        laboratoryRepository.deleteAll();
-        userRepository.deleteAll();
-        groupRepository.deleteAll();
         sessionRepository.deleteAll();
-        groupRepository.flush();
+        if (SET_UP_IS_DONE) {
+            return;
+        }
+        try {
+            String adminAuthority = "ADMIN";
+            Authentication authentication = new JwtAuthentication(true, "password",
+                    List.of(adminAuthority));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        Group group = createGroup(GROUP_NUMBER);
-        Laboratory laboratory1 = createLaboratory(createLaboratoryCreationRequestDto(1));
-        Laboratory laboratory2 = createLaboratory(createLaboratoryCreationRequestDto(2));
-        laboratories.add(laboratory1);
-        laboratories.add(laboratory2);
+            createGroup();
+            LaboratoryDto laboratory1 =
+                    laboratoryTestSetup.createLaboratory(createLaboratoryCreationRequestDto(1));
+            LaboratoryDto laboratory2 =
+                    laboratoryTestSetup.createLaboratory(createLaboratoryCreationRequestDto(2));
+            LABORATORIES.add(laboratory1);
+            LABORATORIES.add(laboratory2);
 
-        User student1 = createUser(createStudentCreationRequestDto('1'), group);
-        User student2 = createUser(createStudentCreationRequestDto('2'), group);
-        students.add(student1);
-        students.add(student2);
+            UserDto student1 = userTestSetup.createUserReturnsUserDto(createStudentCreationRequestDto('1'));
+            UserDto student2 = userTestSetup.createUserReturnsUserDto(createStudentCreationRequestDto('2'));
+            STUDENTS.add(student1);
+            STUDENTS.add(student2);
+            SET_UP_IS_DONE = true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 
     @Test
     public void createSession_ShouldReturnCreated_WhenValidRequest() throws Exception {
-        User student = students.getFirst();
-        Laboratory laboratory = laboratories.getFirst();
+        UserDto student = STUDENTS.getFirst();
+        LaboratoryDto laboratory = LABORATORIES.getFirst();
 
-        var sessionDto = sessionTestSetup.startSession(new StartSessionRequestDto(student.getId(), laboratory.getId()));
+        var sessionDto = sessionTestSetup.startSession(new StartSessionRequestDto(student.id(), laboratory.id()));
 
         checkSession(sessionDto, sessionTestSetup.getSessionDto(SESSION_USER1_LAB1_DTO));
     }
 
     @Test
     public void makeAttempt_ShouldReturnCreated_WhenValidRequest() throws Exception {
-        User student = students.getFirst();
-        Laboratory laboratory = laboratories.getFirst();
+        UserDto student = STUDENTS.getFirst();
+        LaboratoryDto laboratory = LABORATORIES.getFirst();
 
-        var sessionDto = sessionTestSetup.startSession(new StartSessionRequestDto(student.getId(), laboratory.getId()));
+        var sessionDto = sessionTestSetup.startSession(new StartSessionRequestDto(student.id(), laboratory.id()));
         checkSession(sessionDto, sessionTestSetup.getSessionDto(SESSION_USER1_LAB1_DTO));
 
-        var makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory.getId());
+        var makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory.id());
 
         var attemptDto = sessionTestSetup.makeAttempt(makeAttemptDto, sessionDto.id());
         checkAttempt(attemptDto, sessionTestSetup.getAttemptDto(ATTEMPT1_DTO));
@@ -147,13 +140,13 @@ public class SessionControllerIntegrationTest {
 
     @Test
     public void getAttemptById_ShouldReturnOk_WhenValidRequest() throws Exception {
-        User student = students.getFirst();
-        Laboratory laboratory = laboratories.getFirst();
+        UserDto student = STUDENTS.getFirst();
+        LaboratoryDto laboratory = LABORATORIES.getFirst();
 
-        var sessionDto = sessionTestSetup.startSession(new StartSessionRequestDto(student.getId(), laboratory.getId()));
+        var sessionDto = sessionTestSetup.startSession(new StartSessionRequestDto(student.id(), laboratory.id()));
         checkSession(sessionDto, sessionTestSetup.getSessionDto(SESSION_USER1_LAB1_DTO));
 
-        var makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory.getId());
+        var makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory.id());
 
         var attemptDto1 = sessionTestSetup.makeAttempt(makeAttemptDto, sessionDto.id());
         checkAttempt(attemptDto1, sessionTestSetup.getAttemptDto(ATTEMPT1_DTO));
@@ -176,16 +169,16 @@ public class SessionControllerIntegrationTest {
 
     @Test
     public void getSessionById_ShouldReturnOk_WhenValidRequest() throws Exception {
-        User student1 = students.getFirst();
-        User student2 = students.get(1);
+        UserDto student1 = STUDENTS.getFirst();
+        UserDto student2 = STUDENTS.get(1);
 
-        Laboratory laboratory1 = laboratories.getFirst();
-        Laboratory laboratory2 = laboratories.get(1);
+        LaboratoryDto laboratory1 = LABORATORIES.getFirst();
+        LaboratoryDto laboratory2 = LABORATORIES.get(1);
 
         var sessionDto1 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student1.getId(), laboratory1.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student1.id(), laboratory1.id()));
         var sessionDto2 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student2.getId(), laboratory2.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student2.id(), laboratory2.id()));
 
         checkSession(sessionDto1, sessionTestSetup.getSessionDto(SESSION_USER1_LAB1_DTO));
         checkSession(sessionDto2, sessionTestSetup.getSessionDto(SESSION_USER2_LAB2_DTO));
@@ -205,41 +198,41 @@ public class SessionControllerIntegrationTest {
 
     @Test
     public void getLabSessionForStudent_ShouldReturnOk_WhenValidRequest() throws Exception {
-        User student1 = students.getFirst();
-        User student2 = students.get(1);
+        UserDto student1 = STUDENTS.getFirst();
+        UserDto student2 = STUDENTS.get(1);
 
-        Laboratory laboratory1 = laboratories.getFirst();
-        Laboratory laboratory2 = laboratories.get(1);
+        LaboratoryDto laboratory1 = LABORATORIES.getFirst();
+        LaboratoryDto laboratory2 = LABORATORIES.get(1);
 
         var sessionDto1 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student1.getId(), laboratory1.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student1.id(), laboratory1.id()));
         var sessionDto2 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student2.getId(), laboratory2.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student2.id(), laboratory2.id()));
 
         checkSession(sessionDto1, sessionTestSetup.getSessionDto(SESSION_USER1_LAB1_DTO));
         checkSession(sessionDto2, sessionTestSetup.getSessionDto(SESSION_USER2_LAB2_DTO));
 
-        var makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory1.getId());
+        var makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory1.id());
         sessionTestSetup.makeAttempt(makeAttemptDto, sessionDto1.id());
         sessionTestSetup.makeAttempt(makeAttemptDto, sessionDto1.id());
 
-        makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory2.getId());
+        makeAttemptDto = new MakeAttemptDto(REPOSITORY_URL, BRANCH_NAME, laboratory2.id());
         sessionTestSetup.makeAttempt(makeAttemptDto, sessionDto2.id());
 
         long session1Id = sessionDto1.id();
         long session2Id = sessionDto2.id();
 
         var sessionShortDto1 = sessionTestSetup.getLabSessionForStudent(
-                new GetLabSessionFromStudentRequestDto(student1.getId(), laboratory1.getId()));
+                new GetLabSessionFromStudentRequestDto(student1.id(), laboratory1.id()));
         var sessionShortDto2 = sessionTestSetup.getLabSessionForStudent(
-                new GetLabSessionFromStudentRequestDto(student2.getId(), laboratory2.getId()));
+                new GetLabSessionFromStudentRequestDto(student2.id(), laboratory2.id()));
 
         assertEquals(session1Id, sessionShortDto1.id());
         assertEquals(session2Id, sessionShortDto2.id());
-        assertEquals(laboratory1.getId(), sessionDto1.laboratory().id());
-        assertEquals(laboratory2.getId(), sessionDto2.laboratory().id());
-        assertEquals(student1.getId(), sessionDto1.student().id());
-        assertEquals(student2.getId(), sessionDto2.student().id());
+        assertEquals(laboratory1.id(), sessionDto1.laboratory().id());
+        assertEquals(laboratory2.id(), sessionDto2.laboratory().id());
+        assertEquals(student1.id(), sessionDto1.student().id());
+        assertEquals(student2.id(), sessionDto2.student().id());
 
         checkSessionShort(sessionShortDto1,
                 sessionTestSetup.getSessionShortDto("session/session_short_user1_lab1.json"));
@@ -249,28 +242,28 @@ public class SessionControllerIntegrationTest {
 
     @Test
     public void getUserSessions_ShouldReturnOk_WhenValidRequest() throws Exception {
-        User student1 = students.getFirst();
-        User student2 = students.get(1);
+        UserDto student1 = STUDENTS.getFirst();
+        UserDto student2 = STUDENTS.get(1);
 
-        Laboratory laboratory1 = laboratories.getFirst();
-        Laboratory laboratory2 = laboratories.get(1);
+        LaboratoryDto laboratory1 = LABORATORIES.getFirst();
+        LaboratoryDto laboratory2 = LABORATORIES.get(1);
 
         var sessionDto11 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student1.getId(), laboratory1.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student1.id(), laboratory1.id()));
         var sessionDto22 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student2.getId(), laboratory2.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student2.id(), laboratory2.id()));
         var sessionDto12 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student1.getId(), laboratory2.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student1.id(), laboratory2.id()));
         var sessionDto21 =
-                sessionTestSetup.startSession(new StartSessionRequestDto(student2.getId(), laboratory1.getId()));
+                sessionTestSetup.startSession(new StartSessionRequestDto(student2.id(), laboratory1.id()));
 
         checkSession(sessionDto11, sessionTestSetup.getSessionDto(SESSION_USER1_LAB1_DTO));
         checkSession(sessionDto22, sessionTestSetup.getSessionDto(SESSION_USER2_LAB2_DTO));
         checkSession(sessionDto12, sessionTestSetup.getSessionDto("session/session_user1_lab2.json"));
         checkSession(sessionDto21, sessionTestSetup.getSessionDto("session/session_user2_lab1.json"));
 
-        var user1Sessions = sessionTestSetup.getUserSessions(student1.getId());
-        var user2Sessions = sessionTestSetup.getUserSessions(student2.getId());
+        var user1Sessions = sessionTestSetup.getUserSessions(student1.id());
+        var user2Sessions = sessionTestSetup.getUserSessions(student2.id());
 
         assertEquals(2, user1Sessions.getContent().size());
         assertEquals(2, user2Sessions.getContent().size());
@@ -281,33 +274,23 @@ public class SessionControllerIntegrationTest {
         assertTrue(user2Sessions.getContent().contains(sessionTestMapper.sessionDtoToSessionShortDto(sessionDto22)));
     }
 
-    private Laboratory createLaboratory(LaboratoryCreationRequestDto laboratoryCreationRequestDto) {
-        Laboratory laboratory = laboratoryMapper.laboratoryCreationRequestDtoToLaboratory(laboratoryCreationRequestDto);
-        return laboratoryRepository.save(laboratory);
-    }
-
-    private User createUser(UserCreationRequestDto userCreationRequestDto, Group group) {
-        User user = userMapper.userCreationRequestDtoToUser(userCreationRequestDto);
-        user.setGroups(Set.of(group));
-        return userRepository.save(user);
-    }
-
-    private Group createGroup(String name) {
+    private void createGroup() {
         Group group = new Group();
-        group.setGroupName(name);
-        return groupRepository.save(group);
+        group.setGroupName(GROUP_NUMBER);
+        groupRepository.save(group);
     }
 
     private LaboratoryCreationRequestDto createLaboratoryCreationRequestDto(int order) {
-        return new LaboratoryCreationRequestDto(LAB_NAME + order, order,
-                LAB_DESCRIPTION + order, SEMESTER_NUMBER, DEADLINE, IS_HIDDEN, List.of());
+        return new LaboratoryCreationRequestDto("Laboratory" + order, order,
+                "Description" + order, 1, OffsetDateTime.parse("2024-10-07T07:02:27Z"),
+                false, List.of());
 
     }
 
     private UserCreationRequestDto createStudentCreationRequestDto(char order) {
-        return new UserCreationRequestDto(USER_NAME + order,
-                FIRST_NAME + order,
-                SECOND_NAME + order,
+        return new UserCreationRequestDto("username" + order,
+                "firstName" + order,
+                "secondName" + order,
                 GROUP_NUMBER, RoleEnum.STUDENT);
 
     }

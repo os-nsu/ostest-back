288d6ec0de6404e9c4b6820a23a8d46bda1f1777
MariiaKataeva <mariiakataeva@gmail.com>
2024-12-19 04:52:24 +0700

    refs #12943 fixed tests for group

--- src/test/java/ru/nsu/ostest/group/GroupControllerTest.java
+++ src/test/java/ru/nsu/ostest/group/GroupControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import ru.nsu.ostest.TransactionalHelper;
 import ru.nsu.ostest.adapter.in.rest.model.filter.*;
-import ru.nsu.ostest.adapter.in.rest.model.group.GroupCreationRequestDto;
-import ru.nsu.ostest.adapter.in.rest.model.group.GroupDto;
-import ru.nsu.ostest.adapter.in.rest.model.group.GroupEditionRequestDto;
-import ru.nsu.ostest.adapter.in.rest.model.group.GroupResponse;
+import ru.nsu.ostest.adapter.in.rest.model.group.*;
 import ru.nsu.ostest.adapter.in.rest.model.user.auth.JwtResponse;
 import ru.nsu.ostest.adapter.in.rest.model.user.password.UserPasswordDto;
 import ru.nsu.ostest.adapter.in.rest.model.user.role.RoleEnum;
@@ -35,26 +32,20 @@ import ru.nsu.ostest.domain.repository.GroupRepository;
 import ru.nsu.ostest.domain.repository.UserRepository;
 import ru.nsu.ostest.test.TestTestSetup;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-//@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, statements = "delete from test")
 @Testcontainers
 @AutoConfigureMockMvc(addFilters = true)
 @Import({TestTestSetup.class})
 @SpringBootTest(properties = {
-//        "logging.level.sql=trace",
-//        "logging.level.org.hibernate.orm.jdbc.bind=trace",
 })
 @Slf4j
 public class GroupControllerTest {
@@ -67,25 +58,108 @@ public class GroupControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @Spy
     private ObjectMapper objectMapper;
 
     @Autowired
     private GroupRepository groupRepository;
 
     @Autowired
-    private TransactionalHelper transactionalHelper;
-    @Autowired
     private UserRepository userRepository;
 
     @BeforeEach
     void setUp() {
+        userRepository.deleteAll();
         groupRepository.deleteAll();
     }
 
+
     @Test
-    void test_addUserToGroup() throws Exception {
-        GroupCreationRequestDto groupCreationRequestDto = new GroupCreationRequestDto("GROUP111", false);
+    void testUserInGroupCreation_addsUserInGroup_ifCreateUserWithExistingGroupNameParameter() throws Exception {
+        String groupName = "AAAAgroup1";
+
+        GroupDto groupDto = createGroup(groupName);
+        UserPasswordDto userPasswordDto = createUser(groupName, "user1");
+        String accessToken = loginUser(userPasswordDto);
+        UserDto userDto = getUserByAccessToken(accessToken);
+
+        assertEquals(groupDto, userDto.group());
+    }
+
+    @Test
+    void testDeleteGroup_deletesUserFromGroup_ifGroupDeleted() throws Exception {
+        String groupName = "AAAAgroup1";
+
+        GroupDto groupDto = createGroup(groupName);
+        Long groupId = groupDto.id();
+        UserPasswordDto userPasswordDto = createUser(groupName, "user1");
+        String accessToken = loginUser(userPasswordDto);
+        UserDto userDto = getUserByAccessToken(accessToken);
+
+        assertEquals(groupDto, userDto.group());
+
+        deleteGroup(groupId);
+
+        String content = getDeletedGroupByIdMessage(groupId);
+        assertTrue(content.contains(String.format("Group with id '%s' was not found", groupId)));
+
+        UserDto userDto2 = getUserByAccessToken(accessToken);
+        assertEquals(userDto2.id(), userDto.id());
+        assertNull(userDto2.group());
+    }
+
+    @Test
+    void testEditGroup_editsGroupAndUsers_ifGroupEdited() throws Exception {
+        String groupName1 = "AAAAgroup1";
+        String groupName2 = "AAAAgroup2";
+
+        GroupDto group1Dto = createGroup(groupName1);
+        Long group1Id = group1Dto.id();
+        GroupDto group2Dto = createGroup(groupName2);
+        Long group2Id = group2Dto.id();
+
+        UserPasswordDto userPasswordDto1 = createUser(groupName1, "user1");
+        String accessToken1 = loginUser(userPasswordDto1);
+        UserDto userDto1 = getUserByAccessToken(accessToken1);
+
+        UserPasswordDto userPasswordDto2 = createUser(groupName2, "user2");
+        String accessToken2 = loginUser(userPasswordDto2);
+        UserDto userDto2 = getUserByAccessToken(accessToken2);
+
+        assertEquals(group1Dto, userDto1.group());
+        assertEquals(group2Dto, userDto2.group());
+
+        GroupDto editedGroup1Dto = editGroup(group1Id, "new name 1", new HashSet<>(List.of(userDto1.id())), new HashSet<>());
+        GroupDto editedGroup2Dto = editGroup(group2Id, "new name 2", new HashSet<>(List.of(userDto2.id())), new HashSet<>(List.of(userDto1.id())));
+        UserDto updatedUser1Dto = getUserByAccessToken(accessToken1);
+        UserDto updatedUser2Dto = getUserByAccessToken(accessToken2);
+
+        assertNotEquals(editedGroup1Dto, group1Dto);
+        assertNotEquals(editedGroup2Dto, group2Dto);
+
+        assertEquals(updatedUser1Dto.group(), editedGroup2Dto);
+        assertNull(updatedUser2Dto.group());
+    }
+
+    private GroupDto editGroup(Long groupId, String newGroupName, Set<Long> usersToDelete, Set<Long> usersToAdd) throws Exception {
+
+        GroupEditionRequestDto groupEditionRequestDto = new GroupEditionRequestDto(groupId, newGroupName, false, usersToAdd, usersToDelete);
+
+        MockHttpServletResponse response = mockMvc.perform(put("/api/group")
+                        .with(admin())
+                        .content(objectMapper.writeValueAsString(groupEditionRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse();
+
+        GroupDto groupDto = objectMapper.readValue(response.getContentAsByteArray(), GroupDto.class);
+
+        return groupDto;
+    }
+
+    private GroupDto createGroup(String groupName) throws Exception {
+        GroupCreationRequestDto groupCreationRequestDto = new GroupCreationRequestDto(groupName, false);
         MockHttpServletResponse response = mockMvc.perform(post("/api/group")
                         .with(admin())
                         .content(objectMapper.writeValueAsString(groupCreationRequestDto))
@@ -94,10 +168,13 @@ public class GroupControllerTest {
                 .andReturn().getResponse();
 
         GroupDto groupDto = objectMapper.readValue(response.getContentAsByteArray(), GroupDto.class);
-        Long groupId = groupDto.id();
 
-        UserCreationRequestDto dto = new UserCreationRequestDto("user1", "First", "Second", "ZZZ", RoleEnum.ADMIN);
-        response = mockMvc.perform(post("/api/user/registration")
+        return groupDto;
+    }
+
+    private UserPasswordDto createUser(String groupName, String username) throws Exception {
+        UserCreationRequestDto dto = new UserCreationRequestDto(username, "First", "Second", groupName, RoleEnum.ADMIN);
+        MockHttpServletResponse response = mockMvc.perform(post("/api/user/registration")
                         .with(admin())
                         .content(objectMapper.writeValueAsString(dto))
                         .contentType(MediaType.APPLICATION_JSON)
@@ -107,7 +184,11 @@ public class GroupControllerTest {
 
         UserPasswordDto userPasswordDto = objectMapper.readValue(response.getContentAsByteArray(), UserPasswordDto.class);
 
-        response = mockMvc.perform(post("/api/v1/login")
+        return userPasswordDto;
+    }
+
+    private String loginUser(UserPasswordDto userPasswordDto) throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(post("/api/v1/login")
                         .content(objectMapper.writeValueAsString(userPasswordDto))
                         .contentType(MediaType.APPLICATION_JSON)
                 )
@@ -117,62 +198,38 @@ public class GroupControllerTest {
         JwtResponse jwtResponse = objectMapper.readValue(response.getContentAsByteArray(), JwtResponse.class);
         String accessToken = jwtResponse.getAccessToken();
 
-        response = mockMvc.perform(get("/api/user/me")
+        return accessToken;
+    }
+
+    private UserDto getUserByAccessToken(String accessToken) throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/api/user/me")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn().getResponse();
         UserDto userDto = objectMapper.readValue(response.getContentAsByteArray(), UserDto.class);
-        Long userId = userDto.id();
 
-        GroupEditionRequestDto groupEditionRequestDto = new GroupEditionRequestDto(groupId, "GROUP222", false, Set.of(2L, userId), null);
-        response = mockMvc.perform(put("/api/group")
-                        .with(admin())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(groupEditionRequestDto))
-                )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse();
+        return userDto;
+    }
 
-        groupDto = objectMapper.readValue(response.getContentAsByteArray(), GroupDto.class);
-
-        log.info("--> show groups 1");
-        transactionalHelper.runInTransaction(this::showGroups);
-        // todo: сделать assert на наличие группы у пользователя
-
-        groupEditionRequestDto = new GroupEditionRequestDto(groupId, "GROUP222", false, null, Set.of(userId));
-        response = mockMvc.perform(put("/api/group")
-                        .with(admin())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(groupEditionRequestDto))
-                )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn().getResponse();
-
-        log.info("--> show groups 2");
-        transactionalHelper.runInTransaction(this::showGroups);
-        // todo: сделать проверку, что пользователь уделён из группы
-
-        response = mockMvc.perform(delete("/api/group/{id}", groupId)
+    private void deleteGroup(Long groupId) throws Exception {
+        mockMvc.perform(delete("/api/group/{id}", groupId)
                         .with(admin())
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn().getResponse();
+    }
 
-        log.info("--> show users");
-        transactionalHelper.runInTransaction(this::showUsers);
-        // todo: проверку
-
-        response = mockMvc.perform(get("/api/group/search")
-                                .param("size", "25")
-//                        .param("page", "3")
-                                .param("sort", "groupName,asc")
-                                .with(admin())
+    private String getDeletedGroupByIdMessage(Long groupId) throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/api/group/{id}", groupId)
+                        .with(admin())
                 )
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().is(400))
                 .andReturn().getResponse();
-        String contentAsString = response.getContentAsString();
-        log.info("=-=-=> {}", contentAsString);
+
+        String content = response.getContentAsString();
+
+        return content;
     }
 
     private void showUsers() {
@@ -190,10 +247,10 @@ public class GroupControllerTest {
     private void showGroups() {
         List<Group> groups = groupRepository.findAll();
         for (Group group : groups) {
-            log.info("group={}, {}", group, group.getGroupName());
+            log.error("group={}, {}", group, group.getGroupName());
             Set<User> users = group.getUsers();
             for (User user : users) {
-                log.info("user={}, {}, {}, {} ", user, user.getUsername(), user.getFirstName(), user.getSecondName());
+                log.error("user={}, {}, {}, {} ", user, user.getUsername(), user.getFirstName(), user.getSecondName());
             }
         }
     }
@@ -214,23 +271,14 @@ public class GroupControllerTest {
 
     @Test
     void searchUser_ShouldReturnStatusOk_WhenRequestHasFilterAndPagination() throws Exception {
-        createGroup(new GroupCreationRequestDto("GROUP111", false));
-        createGroup(new GroupCreationRequestDto("GROUP112", false));
+        createGroup("GROUP111");
+        createGroup("GROUP112");
 
         SearchRequestDto userSearchRequestDto = new SearchRequestDto(new ArrayList<>(Collections.singleton(new Filter("groupName", false, "string", "GROUP"))), new Pagination(1, 1, 0, 0));
         GroupResponse groupResponse = searchUserReturnsUserResponse(userSearchRequestDto);
         assertThat(groupResponse).isNotNull();
         assertThat(groupResponse.groups()).hasSize(1);
         assertThat(groupResponse.groups().getFirst().groupName()).isEqualTo("GROUP111");
-    }
-
-    void createGroup(GroupCreationRequestDto groupCreationRequestDto) throws Exception {
-        mockMvc.perform(post("/api/group")
-                        .with(admin())
-                        .content(objectMapper.writeValueAsString(groupCreationRequestDto))
-                        .contentType(MediaType.APPLICATION_JSON)
-                ).andExpect(status().isOk())
-                .andReturn().getResponse();
     }
 
     public GroupResponse searchUserReturnsUserResponse(SearchRequestDto searchRequest) throws Exception {
@@ -244,7 +292,7 @@ public class GroupControllerTest {
 
     @Test
     void searchUser_ShouldReturnStatusOk_WhenRequestHasFilter() throws Exception {
-        createGroup(new GroupCreationRequestDto("GROUP111", false));
+        createGroup("GROUP111");
 
         SearchRequestDto userSearchRequestDto = new SearchRequestDto(new ArrayList<>(Collections.singleton(new Filter("groupName", false, "string", "111"))), null);
         GroupResponse userResponse = searchUserReturnsUserResponse(userSearchRequestDto);

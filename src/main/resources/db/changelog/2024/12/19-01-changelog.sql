ede1adbb3136ea08bbbe63da7cd03db5311c13ad
MariiaKataeva <mariiakataeva@gmail.com>
2024-12-19 12:34:00 +0700

    refs #14367 fixed tests data in db

---
+++ src/main/resources/db/changelog/2024/12/19-01-changelog.sql
@@ -0,0 +1,142 @@
+UPDATE "test"
+SET code = 'test_successful_make_clean',
+    name = 'Test successful make clean',
+    id = 1
+WHERE code = '1';
+
+UPDATE "test"
+SET code = 'test_successful_compilation',
+    name = 'Test successful compilation',
+    id = 2
+WHERE code = '2';
+
+UPDATE "test"
+SET code = 'test_library_symbols',
+    name = 'Test library symbols',
+    id = 3
+WHERE code = '3';
+
+UPDATE "test"
+SET code = 'test_makefile_exists',
+    name = 'Test makefile exists',
+    id = 4
+WHERE code = '4';
+
+UPDATE "test"
+SET code = 'test_project_structure',
+    name = 'Test project structure',
+    id = 5
+WHERE code = '5';
+
+UPDATE "test"
+SET code = 'test_compilation_without_warnings',
+    name = 'Test compilation without warnings',
+    id = 6
+WHERE code = '6';
+
+UPDATE "test"
+SET code = 'test_run_without_arguments',
+    name = 'Test run without arguments',
+    id = 7
+WHERE code = '7';
+
+UPDATE "test"
+SET code = 'test_run_with_help_argument',
+    name = 'Test run with help argument',
+    id = 8
+WHERE code = '8';
+
+UPDATE "test"
+SET code = 'test_run_with_invalid_arguments',
+    name = 'Test run with invalid arguments',
+    id = 9
+WHERE code = '9';
+
+UPDATE "test"
+SET code = 'test_log_file_creation',
+    name = 'Test log file creation',
+    id = 10
+WHERE code = '10';
+
+UPDATE "test"
+SET code = 'test_log_contains_message',
+    name = 'Test log contains message',
+    id = 11
+WHERE code = '11';
+
+UPDATE "test"
+SET code = 'test_log_messages_in_order',
+    name = 'Test log messages in order',
+    id = 12
+WHERE code = '12';
+
+UPDATE "test"
+SET code = 'test_execution_with_sanitizers',
+    name = 'Test execution with sanitizers',
+    id = 13
+WHERE code = '13';
+
+UPDATE "test"
+SET code = 'test_proxy_termination_on_signal',
+    name = 'Test proxy termination on signal',
+    id = 14
+WHERE code = '14';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_successful_make_clean'
+-- WHERE test_id = '1';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_successful_compilation'
+-- WHERE test_id = '2';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_library_symbols'
+-- WHERE test_id = '3';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_makefile_exists'
+-- WHERE test_id = '4';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_project_structure'
+-- WHERE test_id = '5';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_compilation_without_warnings'
+-- WHERE test_id = '6';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_run_without_arguments'
+-- WHERE test_id = '7';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_run_with_help_argument'
+-- WHERE test_id = '8';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_run_with_invalid_arguments'
+-- WHERE test_id = '9';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_log_file_creation'
+-- WHERE test_id = '10';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_log_contains_message'
+-- WHERE test_id = '11';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_log_messages_in_order'
+-- WHERE test_id = '12';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_execution_with_sanitizers'
+-- WHERE test_id = '13';
+--
+-- UPDATE "test_laboratory_link"
+-- SET test_id = 'test_proxy_termination_on_signal'
+-- WHERE test_id = '14';
+--
+--
+--

58ee458b82a9be987419809a7fc68f81c3634901
anastasiya <nastya050620031n@gmail.com>
2024-12-20 23:01:54 +0700

    refs #13796 Fixed after review

--- src/main/java/ru/nsu/ostest/adapter/in/rest/controller/LaboratoryController.java
+++ src/main/java/ru/nsu/ostest/adapter/in/rest/controller/LaboratoryController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.nsu.ostest.adapter.in.rest.model.laboratory.*;
 import ru.nsu.ostest.domain.service.LaboratoryService;
 import ru.nsu.ostest.security.annotations.AdminOnlyAccess;
-import ru.nsu.ostest.security.annotations.AdminOrTeacherAccess;
 
 import java.util.List;
 
@@ -18,7 +17,6 @@ public class LaboratoryController {
 
     private final LaboratoryService laboratoryService;
 
-    @AdminOrTeacherAccess
     @GetMapping("/{id}")
     public LaboratoryDto getLaboratory(@PathVariable Long id) {
         return laboratoryService.findById(id);
